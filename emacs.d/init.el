80c4537b8122736e37c2176b46a86b09f533c872
Will Farrington <wfarr@github.com>
2014-02-21 15:56:14 -0500

    Prompt to make directories if DNE before-save

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -287,3 +287,11 @@ Don't mess with special buffers."
   (find-file "/Users/wfarr/Dropbox/org/gtd.org"))
 
 (global-set-key (kbd "C-c g") 'gtd)
+
+(add-hook 'before-save-hook
+          (lambda ()
+            (when buffer-file-name
+              (let ((dir (file-name-directory buffer-file-name)))
+                (when (and (not (file-exists-p dir))
+                           (y-or-n-p (format "Directory %s does not exist. Create it?" dir)))
+                  (make-directory dir t))))))
