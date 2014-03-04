8b5118abcc3c0f994a75e1c72f4576a4de9fd786
Will Farrington <wfarr@github.com>
2014-03-04 23:33:45 +1300

    evil-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -317,3 +317,6 @@ Don't mess with special buffers."
                 (when (and (not (file-exists-p dir))
                            (y-or-n-p (format "Directory %s does not exist. Create it?" dir)))
                   (make-directory dir t))))))
+
+(require 'evil)
+(evil-mode)
