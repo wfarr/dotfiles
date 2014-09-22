a8286350b5d8ae57261f2f47b6774603fa1c5557
Will Farrington <wfarrington@digitalocean.com>
2014-09-22 12:47:21 -0700

    Support the Mac fork with better scrolling
    keep keybindings legit

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -305,5 +305,38 @@ Don't mess with special buffers."
 
 (setq helm-buffers-fuzzy-matching 't)
 
+(require 'helm-projectile)
+
 ;; (require 'evil)
 ;; (evil-mode)
+
+;; requires https://github.com/railwaycat/emacs-mac-port
+(defun mac-switch-meta nil
+  "switch meta between Option and Command"
+  (interactive)
+  (if (eq mac-option-modifier nil)
+      (progn
+        (setq mac-option-modifier 'meta)
+        (setq mac-command-modifier 'hyper))
+    (progn
+      (setq mac-option-modifier nil)
+      (setq mac-command-modifier 'meta))))
+
+
+(if (boundp 'mac-mouse-wheel-mode)
+    (progn
+      (global-set-key [(hyper a)] 'mark-whole-buffer)
+      (global-set-key [(hyper v)] 'yank)
+      (global-set-key [(hyper c)] 'kill-ring-save)
+      (global-set-key [(hyper s)] 'save-buffer)
+      (global-set-key [(hyper l)] 'goto-line)
+      (global-set-key [(hyper w)]
+                      (lambda () (interactive) (delete-window)))
+      (global-set-key [(hyper z)] 'undo)
+
+      (global-set-key (kbd "H-t") 'helm-projectile)
+      (global-set-key (kbd "H-b") 'projectile-switch-to-buffer)
+      (mac-mouse-wheel-mode))
+  (progn
+    (global-set-key (kbd "s-t") 'helm-projectile)
+    (global-set-key (kbd "s-b") 'projectile-switch-to-buffer)))
