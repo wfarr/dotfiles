b5fe34e47400a088099cda35ef536fadc8c9395d
Will Farrington <wcfarrington@gmail.com>
2020-06-02 12:26:06 -0400

    use bouk/chruby-fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,9 +4,6 @@ set -g fish_emoji_width 2
 # Hide the fish greeting
 set fish_greeting ""
 
-source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
-
 set -x GPG_TTY (tty)
 set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
