89a7fdce3419017c83fed6eb9b4d8140a45d2083
Will Farrington <wcfarrington@gmail.com>
2024-02-14 15:43:10 -0500

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -4,16 +4,17 @@ set -g fish_emoji_width 2
 # Hide the fish greeting
 set fish_greeting ""
 
-set -x GPG_TTY (tty)
-set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-gpgconf --launch gpg-agent
+
+set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths
+set -g fish_user_paths "/opt/homebrew/sbin" $fish_user_paths
 
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "$HOME/go/bin" $fish_user_paths
 set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
 
-export VAGRANT_VM_CPUS=8
-export VAGRANT_VM_MEMORY=16384
+set -x GPG_TTY (tty)
+set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
 
 alias vim=nvim
 
